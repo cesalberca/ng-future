1aa38d1e3ac8cab3dd9e7465ef8d6867bf43b03c
cesalberca <cesalberca@gmail.com>
2024-03-12 12:05:59 +0100

    Add component

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonComponent {}
