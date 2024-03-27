25dd0f75c718440e6a0d56bde925fc9c764d1a0b
Joan Morató <moratojoan@gmail.com>
2024-03-27 08:31:05 +0100

    add button component

--- src/app/core/components/button/button.component.ts
+++ src/app/core/components/button/button.component.ts
@@ -1,11 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { Component, HostBinding, Input } from '@angular/core'
 
 @Component({
-  selector: 'app-button',
+  selector: 'button[app-button], a[app-button]',
   standalone: true,
   imports: [],
-  templateUrl: './button.component.html',
+  template: `<ng-content></ng-content>`,
   styleUrl: './button.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  @Input({ required: true }) text: string = ''
+  @Input() variant: 'primary' | 'warn' = 'primary'
+
+  @HostBinding('class.custom-btn') get base() {
+    return true
+  }
+  @HostBinding('class.custom-btn--primary') get primary() {
+    return this.variant === 'primary'
+  }
+  @HostBinding('class.custom-btn--warn') get warn() {
+    return this.variant === 'warn'
+  }
+}
