dc306cfce0a9d29cf87f83e85ab239e2f9092587
Joan Morató <moratojoan@gmail.com>
2024-04-01 12:21:42 +0200

    feat(auth): auth mock

--- src/testing/http-mocks/handlers.ts
+++ src/testing/http-mocks/handlers.ts
@@ -1,5 +1,6 @@
+import { authHandler } from './auth.mocks'
 import { habitTasksHandler } from './habit-tasks.handler'
 import { habitsHandler } from './habits.handler'
 import { usersHandler } from './users.handler'
 
-export const handlers = [...habitsHandler, ...usersHandler, ...habitTasksHandler]
+export const handlers = [...authHandler, ...habitsHandler, ...usersHandler, ...habitTasksHandler]
