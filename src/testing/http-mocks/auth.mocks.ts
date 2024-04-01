f5021ff35198e1c29666f479acdf0c227b1d9bf2
Joan Morató <moratojoan@gmail.com>
2024-04-01 17:42:58 +0200

    feat(auth): auth mock admin user

--- src/testing/http-mocks/auth.mocks.ts
+++ src/testing/http-mocks/auth.mocks.ts
@@ -1,10 +1,23 @@
 import { http, HttpResponse } from 'msw'
 import { api } from './api'
 
+const authAdminUser = {
+  email: 'admin@admin.com',
+  password: 'admin123',
+}
 export const authHandler = [
   http.post(api('auth/login'), async ({ request }) => {
-    const data = await request.json()
-    console.log(data)
+    const data = (await request.json()) as { email: string; password: string }
+    const email = data.email
+    const password = data.password
+    if (!email || !password) {
+      return new HttpResponse('Missing email or password', { status: 400 })
+    }
+
+    if (email !== authAdminUser.email || password !== authAdminUser.password) {
+      return new HttpResponse('Invalid Email or Password', { status: 401 })
+    }
+
     return new HttpResponse(null, {
       // headers: {
       //   'Set-Cookie': 'authToken=abc-123',
