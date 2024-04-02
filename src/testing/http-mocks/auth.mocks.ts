447fa2b61bcc373742f6a6001ea6e945659a83be
Joan Morató <moratojoan@gmail.com>
2024-04-02 09:18:26 +0200

    feat(auth): auth mother admin

--- src/testing/http-mocks/auth.mocks.ts
+++ src/testing/http-mocks/auth.mocks.ts
@@ -1,10 +1,7 @@
 import { http, HttpResponse } from 'msw'
 import { api } from './api'
+import { AuthMother } from '../mothers/auth.mother'
 
-const authAdminUser = {
-  email: 'admin@admin.com',
-  password: 'admin123',
-}
 export const authHandler = [
   http.post(api('auth/login'), async ({ request }) => {
     const data = (await request.json()) as { email: string; password: string }
@@ -14,6 +11,7 @@ export const authHandler = [
       return new HttpResponse('Missing email or password', { status: 400 })
     }
 
+    const authAdminUser = AuthMother.admin()
     if (email !== authAdminUser.email || password !== authAdminUser.password) {
       return new HttpResponse('Invalid Email or Password', { status: 401 })
     }
---
+++ src/testing/mothers/auth.mother.ts
@@ -0,0 +1,8 @@
+export class AuthMother {
+  static admin() {
+    return {
+      email: 'admin@admin.com',
+      password: 'admin123',
+    }
+  }
+}
