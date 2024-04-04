cdd004e2c40803544d67fd8cfc14829fd51fab81
cesalberca <cesalberca@gmail.com>
2024-04-04 13:45:35 +0200

    Add base url interceptor

---
+++ src/app/core/http/interceptors/base-url.interceptor.ts
@@ -0,0 +1,6 @@
+import { HttpInterceptorFn } from '@angular/common/http'
+import { environment } from '../../../../environments/environment'
+
+export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req.clone({ url: `${environment.apiBaseUrl}/${req.url}` }))
+}
