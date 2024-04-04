ad7989531c19a17bf0b90ab45bdd778dc83fcdab
cesalberca <cesalberca@gmail.com>
2024-04-04 13:53:41 +0200

    Create page not found

---
+++ src/app/core/components/not-found/not-found-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+
+@Component({
+  selector: 'app-not-found-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './not-found-page.component.html',
+  styleUrl: './not-found-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NotFoundPage {}
