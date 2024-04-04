a00a06ae0a4ff9f078ec75824c57bdf418d6a52d
cesalberca <cesalberca@gmail.com>
2024-04-04 13:45:46 +0200

    Register interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,13 @@ import { ApplicationConfig } from '@angular/core'
 import { provideRouter, withComponentInputBinding } from '@angular/router'
 import { routes } from './app.routes'
 import { provideClientHydration } from '@angular/platform-browser'
-import { provideHttpClient, withFetch } from '@angular/common/http'
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http'
+import { baseUrlInterceptor } from './core/http/interceptors/base-url.interceptor'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([baseUrlInterceptor])),
   ],
 }
