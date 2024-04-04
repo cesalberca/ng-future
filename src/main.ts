41a4b50638a67366c442ef584501764e9070a618
cesalberca <cesalberca@gmail.com>
2024-04-04 14:12:20 +0200

    Rename

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser'
 import { appConfig } from './app/app.config'
 import { AppComponent } from './app/app.component'
-import { browser } from './testing/http-mocks/browser.mocks'
+import { browser } from './tests/http-mocks/browser.mocks'
 import { isDevMode } from '@angular/core'
 
 let mocks = Promise.resolve()
