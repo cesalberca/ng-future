c1d0d9ba628c56eb5b76d08f2dc5d149e81f3bef
cesalberca <cesalberca@gmail.com>
2024-04-04 13:12:15 +0200

    Fix test

--- src/testing/features/render-habits.spec.ts
+++ src/testing/features/render-habits.spec.ts
@@ -1,19 +1,12 @@
 import { expect, test } from '@playwright/test'
+import { HabitMother } from '../mothers/habit.mother'
 
 test.describe('render habits', () => {
   test('names should be rendered', async ({ page }) => {
     await page.goto('/')
-    const habits = [
-      {
-        id: '33c6daf1-491f-4ab1-a6dd-e0198e1bef91',
-        name: 'Read',
-      },
-      {
-        id: '33c6daf1-491f-4ab1-a6dd-e0198e1bef91',
-        name: 'Meditate',
-      },
-    ]
-    const habitsInPage = habits.map(habit => page.getByText(habit.name))
+    const habits = HabitMother.habits()
+    const habitsInPage = habits.map(habit => page.getByRole('link', { name: habit.name }))
+
     for (const habitInPage of habitsInPage) {
       await expect(habitInPage).toBeVisible()
     }
