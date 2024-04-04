4d29ed19347e8c1ac502aa26b9cf54f496ed9bf9
cesalberca <cesalberca@gmail.com>
2024-04-04 10:51:32 +0200

    Fix e2e test

--- src/testing/features/home.spec.ts
+++ src/testing/features/home.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test'
 test('has welcome text', async ({ page }) => {
   await page.goto('/')
 
-  const actual = page.getByText('NG Future')
+  const actual = page.getByText('Hello world')
 
   await expect(actual).toBeVisible()
 })
