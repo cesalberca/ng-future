c816f7ecc9eea78649ebcd590ac2c1572a41b93c
Joan Morató <moratojoan@gmail.com>
2024-04-04 17:20:45 +0200

    enable mocking client & ssr
    (cherry picked from commit 4b7dfc15b6da6beed473dce0cc9af01eec27df93)

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser'
 import { AppComponent } from './app/app.component'
 import { config } from './app/app.config.server'
+import { isDevMode } from '@angular/core'
 
-const bootstrap = () => bootstrapApplication(AppComponent, config)
+async function enableMocking() {
+  if (!isDevMode()) return
+
+  const { server } = await import('./tests/http-mocks/server.mocks')
+  return server.listen()
+}
+
+const bootstrap = async () => {
+  await enableMocking()
+  return bootstrapApplication(AppComponent, config)
+}
 
 export default bootstrap
