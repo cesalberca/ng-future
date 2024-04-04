c10a046982dc0da71c844620d827602dcf3f8110
cesalberca <cesalberca@gmail.com>
2024-04-04 13:45:25 +0200

    Add environment type

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
-export const environment = {
+import { Environment } from '../app/core/environment/environment'
+
+export const environment: Environment = {
   apiBaseUrl: 'http://localhost:4000/api',
 }
