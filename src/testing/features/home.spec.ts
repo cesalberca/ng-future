bb9f3a4202ed03f6871fbf676cabf26f6129594b
mariamoraan <mariamoranluaces@gmail.com>
2024-04-04 12:47:11 +0200

    test(habits): add test to habit headers & doc to readme

--- src/testing/features/home.spec.ts
+++ src/testing/features/home.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '@playwright/test'
+import { HabitMother } from '../mothers/habit.mother'
 
 test('has welcome text', async ({ page }) => {
   await page.goto('/')
@@ -6,4 +7,11 @@ test('has welcome text', async ({ page }) => {
   const actual = page.getByText('NG Future')
 
   await expect(actual).toBeVisible()
+})
+
+test('has habit headers', async ({ page }) => {
+  await page.goto('/')
+  const habits = HabitMother.habits()
+  const habitsInPage = habits.map(habit => page.getByText(habit.name))
+  habitsInPage.forEach(async habitInPage => await expect(habitInPage).toBeVisible())
 })
