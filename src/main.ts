a09458ffab3546ae0bafe87e8f88fc14fbc6a3eb
cesalberca <cesalberca@gmail.com>
2024-04-04 13:22:16 +0200

    Create function

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser'
 import { appConfig } from './app/app.config'
 import { AppComponent } from './app/app.component'
-import { browser } from './testing/http-mocks/browser.mocks'
 import { isDevMode } from '@angular/core'
 
-let mocks = Promise.resolve()
+async function enableMocking() {
+  if (!isDevMode()) {
+    return
+  }
 
-if (isDevMode()) {
-  mocks = browser
-    .start({
-      onUnhandledRequest: 'bypass',
-    })
-    .then()
+  const { browser } = await import('./testing/http-mocks/browser.mocks')
+  return browser.start()
 }
 
-mocks.then(() => {
+enableMocking().then(() => {
   bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err))
 })
