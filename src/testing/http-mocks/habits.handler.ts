942873a8086850ed79e9d3f0e7116cb99a75e955
cesalberca <cesalberca@gmail.com>
2024-04-04 13:53:21 +0200

    Register new endpoint

--- src/testing/http-mocks/habits.handler.ts
+++ src/testing/http-mocks/habits.handler.ts
@@ -1,23 +1,21 @@
 import { http, HttpResponse } from 'msw'
 import { api } from './api'
-import { Habit } from '../../app/habits/habit'
+import { Habit } from '../../app/features/habits/habit'
+import { Id } from '../../app/core/models/id'
+import { HabitMother } from '../mothers/habit.mother'
 
 export const habitsHandler = [
-  http.get<never, never, Habit[]>(api('habits'), () =>
+  http.get<{ id: Id }, never, Habit>(api('habits/:id'), ({ params }) =>
     HttpResponse.json(
-      [
-        {
-          id: '33c6daf1-491f-4ab1-a6dd-e0198e1bef91',
-          name: 'Read',
-        },
-        {
-          id: '33c6daf1-491f-4ab1-a6dd-e0198e1bef91',
-          name: 'Meditate',
-        },
-      ],
+      HabitMother.habits().find(habit => habit.id === params.id),
       {
         status: 200,
       },
     ),
+  ),
+  http.get<never, never, Habit[]>(api('habits'), () =>
+    HttpResponse.json(HabitMother.habits(), {
+      status: 200,
+    }),
   ),
 ]
