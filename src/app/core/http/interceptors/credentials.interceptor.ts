1827942e62bc7a02ef8edc28f04dc6552c9d5fb9
Joan Morató <moratojoan@gmail.com>
2024-04-05 13:49:38 +0200

    with auth function comment: ssr vs client cookies issue

--- src/app/core/http/interceptors/credentials.interceptor.ts
+++ src/app/core/http/interceptors/credentials.interceptor.ts
@@ -1,7 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http'
 
 export const credentialsInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log('hola')
   return next(
     req.clone({
       withCredentials: true,
--- src/tests/http-mocks/middlewares/withAuth.ts
+++ src/tests/http-mocks/middlewares/withAuth.ts
@@ -8,9 +8,15 @@ export function withAuth<
   resolver: HttpResponseResolver<Params, RequestBodyType, ResponseBodyType>,
 ): HttpResponseResolver<Params, RequestBodyType, ResponseBodyType> {
   return async input => {
-    const { cookies } = input
-    console.log('withAuth cookies', cookies)
-
+    // NOTE: At the moment this code only works with client side requests.
+    // Github issue: https://github.com/angular/angular-cli/issues/26323#issuecomment-1817475549
+    // Github issue: https://github.com/angular/angular/issues/15730
+    //
+    // const { cookies } = input
+    // if (!cookies[AUTH_TOKEN_KEY]) {
+    //   throw new HttpResponse(undefined, { status: 401 })
+    // }
+    //
     return resolver(input)
   }
 }
