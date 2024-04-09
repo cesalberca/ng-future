ab7782eb177701e0f3cd3d90deb55c83286555b0
Joan Morató <moratojoan@gmail.com>
2024-04-09 17:44:46 +0200

    checkbox with using model to propagate the change

--- src/app/core/components/checkbox/checkbox.component.ts
+++ src/app/core/components/checkbox/checkbox.component.ts
@@ -1,20 +1,15 @@
-import { ChangeDetectionStrategy, Component, model, output } from '@angular/core'
+import { ChangeDetectionStrategy, Component, model } from '@angular/core'
 import { FormsModule } from '@angular/forms'
-import { CheckboxChangeEvent, CheckboxModule } from 'primeng/checkbox'
+import { CheckboxModule } from 'primeng/checkbox'
 
 @Component({
   selector: 'app-checkbox',
   standalone: true,
   imports: [FormsModule, CheckboxModule],
-  template: ` <p-checkbox [(ngModel)]="checked" [binary]="true" (onChange)="handleChange($event)"></p-checkbox> `,
+  template: ` <p-checkbox [(ngModel)]="checked" [binary]="true"></p-checkbox> `,
   styleUrl: './checkbox.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CheckboxComponent {
   checked = model<boolean>()
-  onChange = output<boolean>()
-
-  handleChange(event: CheckboxChangeEvent) {
-    this.onChange.emit(event.checked)
-  }
 }
