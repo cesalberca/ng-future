3e6e46d8df7f890f278c562742d0f4494336c2db
Joan Morató <moratojoan@gmail.com>
2024-04-18 12:23:32 +0200

    fix create habit test e2e
    - adding aria label to forms to get forms with role=form
    - page.locator to getByText

--- src/tests/features/create-habit.e2e.ts
+++ src/tests/features/create-habit.e2e.ts
@@ -4,11 +4,12 @@ test.describe('create habit', () => {
   test('habit should be created', async ({ page }) => {
     await page.goto('/')
 
+    const newHabitName = 'New habit'
     await page.getByTestId('create-habit').click()
-    await page.getByLabel('Name').fill('New habit')
+    await page.getByLabel('Name').fill(newHabitName)
     await page.getByRole('form').dispatchEvent('submit')
 
     await page.goto('/')
-    expect(page.locator).toContain('New habit')
+    expect(page.getByText(newHabitName)).toBeTruthy()
   })
 })
