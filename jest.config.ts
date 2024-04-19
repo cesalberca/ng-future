394d8fb4e496489a38b0b44bc34afc7076d4f1f0
cesalberca <cesalberca@gmail.com>
2024-04-19 10:49:15 +0200

    Add coverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,9 +2,10 @@ import { Config } from 'jest'
 
 const config: Config = {
   preset: 'jest-preset-angular',
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov'],
   setupFilesAfterEnv: ['<rootDir>/setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
-
   transform: {
     '^.+\\.ts?$': 'jest-preset-angular',
     '^.+\\.tsx?$': 'jest-preset-angular',
